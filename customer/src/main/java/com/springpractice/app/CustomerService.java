f0f749cf6961308430775291626d5e5b64e4b0a6
rinchan_01 <thailinh8426@gmail.com>
2024-02-23 23:20:32 +0700

    update eureka server application, update dependencies

--- customer/src/main/java/com/springpractice/app/CustomerService.java
+++ customer/src/main/java/com/springpractice/app/CustomerService.java
@@ -5,29 +5,32 @@ import org.springframework.web.client.RestTemplate;
 
 import com.springpractice.app.entity.Customer;
 
+import lombok.AllArgsConstructor;
+
 //handle requests from the controller
 @Service
-public record CustomerService(CustomerRepository customerRepository, RestTemplate restTemplate) {
+@AllArgsConstructor
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
+
     @SuppressWarnings("null")
     public void registerCustomer(CustomerRegistrationRequest customerRequest) {
-        //throw new UnsupportedOperationException("Not implemented yet");
-        Customer customer = Customer.builder().
-            firstName(customerRequest.firstName()).
-            lastName(customerRequest.lastName()).
-            email(customerRequest.email()).
-            build();
-        //todo: check if email is valid
-        //todo: check if email is already taken
-        //todo: check if fraudster
+        // throw new UnsupportedOperationException("Not implemented yet");
+        Customer customer = Customer.builder().firstName(customerRequest.firstName())
+                .lastName(customerRequest.lastName()).email(customerRequest.email()).build();
+        // store the customer in the database
+        customerRepository.saveAndFlush(customer);
+        // todo: check if email is valid
+        // todo: check if email is already taken
+        // todo: check if fraudster
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-            "http:localhost:8081/api/v1/fraud-check/{customerId}", FraudCheckResponse.class, customer.getId());
-        if (fraudCheckResponse.isFraudster()){
+                "http://localhost:8081/api/v1/fraud-check/{customerId}", FraudCheckResponse.class, customer.getId());
+        if (fraudCheckResponse.isFraudster()) {
             throw new IllegalStateException("Customer is a fraudster");
         }
-        //store the customer in the database
-       customerRepository.save(customer);
-        // todo: send notification 
 
+        // todo: send notification
 
     }
 }
