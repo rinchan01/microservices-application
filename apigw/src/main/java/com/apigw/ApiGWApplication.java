78149443723da42f0747d03ef6e1842c59c43480
rinchan_01 <thailinh8426@gmail.com>
2024-03-22 10:35:18 +0700

    config apigw

---
+++ apigw/src/main/java/com/apigw/ApiGWApplication.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGWApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGWApplication.class, args);
+    }
+}
